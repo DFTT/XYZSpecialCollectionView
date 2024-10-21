15fb3157daaccbf899ce6596b09d0659e917ce61
dadadongl <lidong@021.com>
2024-10-21 18:06:46 +0800

    collectionView横向滚动时兼容设置对齐方式

--- XYZSpecialCollectionView/CollectionViewLayouts/AlignCollectionViewFollowLayout.swift
+++ XYZSpecialCollectionView/CollectionViewLayouts/AlignCollectionViewFollowLayout.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+// 主要支持纵向滚动的.
+//  对于横向滚动的, 仅对只有一行cell场景才有效
 class AlignCollectionViewFollowLayout: UICollectionViewFlowLayout {
     enum AlignType {
         case center
@@ -18,7 +20,72 @@ class AlignCollectionViewFollowLayout: UICollectionViewFlowLayout {
     
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         guard let atts = super.layoutAttributesForElements(in: rect) else { return nil }
+        guard atts.isEmpty == false else { return atts }
         
+        // 横向滚动
+        if scrollDirection == .horizontal {
+            guard let collectionView = collectionView else {
+                return atts
+            }
+            if collectionViewContentSize.width >= collectionView.bounds.size.width {
+                return atts
+            }
+            if case .left = align {
+                return atts
+            }
+            
+            // 只有一行时才生效
+            let firstAtt = atts.first!
+            let otherLineArtt = atts.first { att in
+                if att.frame.minY > firstAtt.frame.minY {
+                    return att.frame.minY - firstAtt.frame.minY > firstAtt.frame.size.height
+                } else {
+                    return firstAtt.frame.minY - att.frame.minY > att.frame.size.height
+                }
+            }
+            if otherLineArtt != nil {
+                assertionFailure("横向滚动 且 超过1行 不支持")
+                return atts
+            }
+            
+            let padding: UIEdgeInsets = sectionInset
+            
+            if case .right = align {
+                var suffixAtt: UICollectionViewLayoutAttributes? = nil
+                for att in atts.reversed() {
+                    if suffixAtt == nil || att.frame.minY != suffixAtt!.frame.minY {
+                        var newFrame = att.frame
+                        newFrame.origin.x = collectionView.bounds.size.width - padding.right - newFrame.size.width
+                        att.frame = newFrame
+                        suffixAtt = att
+                        continue
+                    }
+                    // 同一行
+                    var newFrame = att.frame
+                    newFrame.origin.x = suffixAtt!.frame.minX - minimumInteritemSpacing - newFrame.size.width
+                    att.frame = newFrame
+                    suffixAtt = att
+                }
+                return atts
+            }
+            
+            if case .center = align {
+                let sumW = atts.reduce(CGFloat(0)) { $0 + $1.frame.size.width }
+                let sumSpace = CGFloat(atts.count - 1) * minimumInteritemSpacing
+                var beginX = (collectionView.bounds.size.width - sumW - sumSpace) / CGFloat(2)
+                for attEle in atts {
+                    var new = attEle.frame
+                    new.origin.x = beginX
+                    attEle.frame = new
+                    beginX = new.maxX + minimumInteritemSpacing
+                }
+            }
+            
+            return atts
+        }
+        
+        // 纵向滚动
+       
         // 居中对齐
         if case .center = align {
             func __tryMakeAlignCenter(_ seamLineAtts: [UICollectionViewLayoutAttributes]) {
@@ -27,14 +94,13 @@ class AlignCollectionViewFollowLayout: UICollectionViewFlowLayout {
                 let sumW = seamLineAtts.reduce(CGFloat(0)) { $0 + $1.frame.size.width }
                 let sumSpace = CGFloat(seamLineAtts.count - 1) * minimumInteritemSpacing
                 var beginX = (collectionViewContentSize.width - sumW - sumSpace) / CGFloat(2)
-                seamLineAtts.forEach { attEle in
+                for attEle in seamLineAtts {
                     var new = attEle.frame
                     new.origin.x = beginX
                     attEle.frame = new
                     beginX = new.maxX + minimumInteritemSpacing
                 }
             }
-            
             var seamLineAtts = [UICollectionViewLayoutAttributes]()
             var preAtt: UICollectionViewLayoutAttributes? = nil
             for att in atts {
--- XYZSpecialCollectionView/Examples/AlignCollectionViewController.swift
+++ XYZSpecialCollectionView/Examples/AlignCollectionViewController.swift
@@ -18,14 +18,28 @@ class AlignCollectionViewController: UIViewController {
         view.addSubview(collcetionView3)
 
         let rect = view.bounds
-        var rect1 = CGRect(x: 0, y: 150, width: rect.size.width, height: 180)
+        var rect1 = CGRect(x: 0, y: 100, width: rect.size.width, height: 150)
         collcetionView.frame = rect1
-        rect1.origin.y += 200
+        rect1.origin.y += 160
         collcetionView2.frame = rect1
-        rect1.origin.y += 200
+        rect1.origin.y += 160
         collcetionView3.frame = rect1
+
+        shortCollectionView.insert(collcetionView4)
+        shortCollectionView.insert(collcetionView5)
+
+        view.addSubview(collcetionView4)
+        rect1.origin.y += 160
+        rect1.size.height = 40
+        collcetionView4.frame = rect1
+
+        view.addSubview(collcetionView5)
+        rect1.origin.y += 50
+        rect1.size.height = 40
+        collcetionView5.frame = rect1
     }
 
+    private var shortCollectionView = Set<UICollectionView>()
     private let dataTexts: [String] = [
         "一",
         "一二",
@@ -42,6 +56,13 @@ class AlignCollectionViewController: UIViewController {
         "一二",
         "六七八九",
         "一二",
+    ]
+
+    private var shortDataTexts: [String] = [
+        "1",
+        "12",
+        "123",
+        "1234",
     ]
 
     private lazy var collcetionView: UICollectionView = {
@@ -85,34 +106,74 @@ class AlignCollectionViewController: UIViewController {
         view.backgroundColor = .yellow
         return view
     }()
+
+    private lazy var collcetionView4: UICollectionView = {
+        let layout = AlignCollectionViewFollowLayout()
+        layout.align = .center
+        layout.minimumInteritemSpacing = 10
+        layout.scrollDirection = .horizontal
+
+        let view = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        view.delegate = self
+        view.dataSource = self
+        view.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        view.backgroundColor = .orange
+        return view
+    }()
+
+    private lazy var collcetionView5: UICollectionView = {
+        let layout = AlignCollectionViewFollowLayout()
+        layout.align = .right
+        layout.minimumInteritemSpacing = 10
+        layout.scrollDirection = .horizontal
+
+        let view = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        view.delegate = self
+        view.dataSource = self
+        view.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        view.backgroundColor = .orange
+        return view
+    }()
 }
 
 extension AlignCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dataTexts.count
+        return shortCollectionView.contains(collectionView) ? shortDataTexts.count : dataTexts.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
-        cell.label.text = dataTexts[indexPath.item]
+        cell.label.text = shortCollectionView.contains(collectionView) ? shortDataTexts[indexPath.item] : dataTexts[indexPath.item]
+        cell.label.backgroundColor = UIColor(red: CGFloat(Float.random(in: 0 ... 1)),
+                                             green: CGFloat(Float.random(in: 0 ... 1)),
+                                             blue: CGFloat(Float.random(in: 0 ... 1)), alpha: 1)
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let text = (dataTexts[indexPath.item] as NSString)
-        var size = text.boundingRect(with: CGSize(width: collectionView.bounds.size.width, height: 30),
-                                     attributes: [.font: UIFont.systemFont(ofSize: 20)],
-                                     context: nil).size
+        let text = shortCollectionView.contains(collectionView) ? shortDataTexts[indexPath.item] : dataTexts[indexPath.item]
+
+        var size = (text as NSString).boundingRect(with: CGSize(width: collectionView.bounds.size.width, height: 30),
+                                                   attributes: [.font: UIFont.systemFont(ofSize: 20)],
+                                                   context: nil).size
         size.width += 10
         return size
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if shortCollectionView.contains(collectionView) {
+            shortDataTexts.append("33")
+            collectionView.reloadData()
+            collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .left, animated: true)
+            return
+        }
     }
 }
 
 class CollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.contentView.addSubview(label)
-        label.frame = self.contentView.bounds
+        contentView.addSubview(label)
     }
 
     @available(*, unavailable)
@@ -120,11 +181,15 @@ class CollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = contentView.bounds
+    }
+
     lazy var label: UILabel = {
         let label = UILabel()
         label.textColor = UIColor.black
         label.font = UIFont.systemFont(ofSize: 20)
-        label.backgroundColor = .blue
         label.textAlignment = .center
         return label
     }()
